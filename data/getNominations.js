1dc87e534ba6bcd93414ac654b1f85779952a41f
maidersonn <maidersonn@gmail.com>
2022-06-12 23:02:47 +0200

    feat: add query to get nominations

---
+++ data/getNominations.js
@@ -0,0 +1,16 @@
+const { sql } = require("slonik");
+
+module.exports = async (db) => {
+    try {
+        const result = await db.query(sql`
+            SELECT id, email, description, involvement, talent, referrer, dataReferrer, status
+            FROM referral
+            WHERE status = 'pending'
+      `);
+        return result.rows;
+    } catch (error) {
+        console.info("Error at geTNominations query: ", error.message);
+        return false;
+    }
+};
+
