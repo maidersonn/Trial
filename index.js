23fab558fa223b11b47f2f030beb995641483355
maidersonn <maidersonn@gmail.com>
2022-06-16 11:05:46 +0200

    feat: add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+var cors = require('cors')
 const app = express();
 const db = require("./config/db");
 
+app.use(cors())
 app.use(express.json());
 
 app.use(require("./controllers")(db));
